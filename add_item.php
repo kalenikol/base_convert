121465d4d84b410357b09a907ecca33bf012e753
Anton Kalenik <kalenik.online@gmail.com>
2016-07-31 14:02:22 +0200

    сделал добавление новых наименований в базу

---
+++ add_item.php
@@ -0,0 +1,44 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_LOGIN', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'items');
+$link = mysqli_connect(DB_HOST, DB_LOGIN, DB_PASSWORD, DB_NAME);
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+//    echo $_SERVER['REQUEST_METHOD'];
+    $oldName = $_POST['name_1'];
+//    echo $oldName;
+    $oldCode = $_POST['code_1'];
+    $newName = $_POST['name_2'];
+    $newCode = $_POST['code_2'];
+    if(!$_POST['enabled']){
+        $enabled = "n";
+    } else {
+        $enabled = "y";
+    }
+    $e_link = $_POST['e_link'];
+    $sql = "INSERT INTO goods (name_1, code_1, name_2, code_2, enabled, link) VALUES ('$oldName', '$oldCode', '$newName', '$newCode', '$enabled', '$e_link');";
+    $result = mysqli_query($link, $sql);
+    mysqli_close($link);
+    ?>
+        <a href="index.php">Домой</a>
+    <?php
+    exit();
+}
+?>
+<!--TODO проверка полей на заполненность-->
+    <form  method='POST' action="<?php $_SERVER['REQUEST_URI'] ?>">
+        <label for="label_name_1">Имя в базе 1С</label>
+        <input type="text" name="name_1" id="label_name_1">
+        <label for="label_code_1">Артикул в базе 1С</label>
+        <input type="text" name="code_1" id="label_code_1"><br>
+        <label for="label_name_2">Имя в выгруке</label>
+        <input type="text" name="name_2" id="label_name_2">
+        <label for="label_code_2">Артикул в выгрузке</label>
+        <input type="text" name="code_2" id="label_code_2"><br>
+        <label for="label_enabled">Выгружать?</label>
+        <input type="checkbox" checked name="enabled" value="enabled" id="label_enabled"><br>
+        <label for="label_link">Ссылка в ешопе</label>
+        <input type="text" name="e_link" id="label_link"><br>
+        <input type="submit" name="submit" value="go!">
+    </form>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Главная страница</title>
+</head>
+<body>
+<ul>
+    <li><a href="add_item.php">Добавление позиции</a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
+</body>
+</html>
